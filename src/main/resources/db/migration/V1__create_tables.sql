085b4335574901451bd948c7a991f9108a7f0f84
TrueMerc <true_merc@mail.ru>
2020-07-12 20:24:05 +0300

    Изменена схема базы данных. Теперь сотрудники относятся к адресам как многие к одному.

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE employees
     first_name varchar(40) NOT NULL,
     patronymic varchar(40) NOT NULL,
     work_hours_id bigint NOT NULL,
+    address_id bigint NOT NULL,
     age smallint NOT NULL
 );
 -- Таблица, содержащая административные округа.
@@ -16,45 +17,45 @@ CREATE TABLE administrative_divisions
     name varchar(50) NOT NULL
 );
 /* Таблица, содержащая список районов, при решении
- * задачи использовалось предположение, что районы
- * могут принадлежать к округам, а могут быть самостоятельными,
- * тогда в соответствующем поле для идентификатора округа возникает
- * значение null. Округа без районов быть не может.
- */
- DROP TABLE IF EXISTS districts;
- CREATE TABLE districts
- (
+* задачи использовалось предположение, что районы
+* могут принадлежать к округам, а могут быть самостоятельными,
+* тогда в соответствующем поле для идентификатора округа возникает
+* значение null. Округа без районов быть не может.
+*/
+DROP TABLE IF EXISTS districts;
+CREATE TABLE districts
+(
     id bigserial PRIMARY KEY,
     name varchar(50) NOT NULL,
     division_id bigint
- );
- -- Таблица, содержащая адреса
- DROP TABLE IF EXISTS addresses;
- CREATE TABLE addresses
- (
+);
+-- Таблица, содержащая адреса
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses
+(
     id bigserial PRIMARY KEY,
     name varchar(60) NOT NULL,
     district_id bigint NOT NULL
- );
- -- Таблица, содержащая возможные интервалы рабочего времени.
- DROP TABLE IF EXISTS work_hours;
- CREATE TABLE work_hours
- (
+);
+-- Таблица, содержащая возможные интервалы рабочего времени.
+DROP TABLE IF EXISTS work_hours;
+CREATE TABLE work_hours
+(
     id bigserial PRIMARY KEY,
     start_time time NOT NULL,
     end_time time NOT NULL
- );
+);
  -- Таблица, устанавливающая соответствие между сотрудниками и адресами
- DROP TABLE IF EXISTS employees_addresses;
- CREATE TABLE employees_addresses(
-    employee_id bigint,
-    address_id bigint,
-    PRIMARY KEY( employee_id, address_id ),
-    FOREIGN KEY (employee_id)
-        REFERENCES employees (id),
-    FOREIGN KEY (address_id)
-        REFERENCES  addresses (id)
- );
+--  DROP TABLE IF EXISTS employees_addresses;
+--  CREATE TABLE employees_addresses(
+--     employee_id bigint,
+--     address_id bigint,
+--     PRIMARY KEY( employee_id, address_id ),
+--     FOREIGN KEY (employee_id)
+--         REFERENCES employees (id),
+--     FOREIGN KEY (address_id)
+--         REFERENCES  addresses (id)
+--  );
 
 
 
--- src/main/resources/db/migration/V2__fill_tables.sql
+++ src/main/resources/db/migration/V2__fill_tables.sql
@@ -24,26 +24,26 @@ INSERT INTO work_hours (start_time, end_time)
            ('09:00:00', '18:00:00'),
            ('10:00:00', '19:00:00');
 -- Тестовые значения для работников
-INSERT INTO employees (second_name, first_name, patronymic, work_hours_id, age)
-    VALUES ('Ivanov', 'Ivan', 'Ivanovich', 1, 25),
-           ('Petrov', 'Petr', 'Petrovich', 2, 34),
-           ('Sidorov', 'Petr', 'Grigorievich', 3, 38),
-           ('Smirnov', 'Ivan', 'Ivanovich', 1, 27),
-           ('Kuznetsov', 'Vladimir', 'Nikolaevich', 3, 35),
-           ('Nikolaev', 'Ivan', 'Petrovich', 2, 30),
-           ('Ivanov', 'Petr', 'Vladimirovich', 2, 29),
-           ('Smirnov', 'Vladimir', 'Ivanovich', 3, 29),
-           ('Sidorov', 'Nikolay', 'Petrovich', 1, 31);
+INSERT INTO employees (second_name, first_name, patronymic, work_hours_id, address_id, age)
+    VALUES ('Ivanov', 'Ivan', 'Ivanovich', 1, 1, 25),
+           ('Petrov', 'Petr', 'Petrovich', 2, 2, 34),
+           ('Sidorov', 'Petr', 'Grigorievich', 3, 3, 38),
+           ('Smirnov', 'Ivan', 'Ivanovich', 1, 4, 27),
+           ('Kuznetsov', 'Vladimir', 'Nikolaevich', 3, 5, 35),
+           ('Nikolaev', 'Ivan', 'Petrovich', 2, 6, 30),
+           ('Ivanov', 'Petr', 'Vladimirovich', 2, 7, 29),
+           ('Smirnov', 'Vladimir', 'Ivanovich', 3, 8, 29),
+           ('Sidorov', 'Nikolay', 'Petrovich', 1, 3, 31);
 -- Тестовые значения для пар "работник - адрес проживания"
-INSERT INTO employees_addresses (employee_id, address_id)
-    VALUES (1, 1),
-           (2, 2),
-           (3, 3),
-           (4, 4),
-           (5, 5),
-           (6, 6),
-           (7, 7),
-           (8, 8),
-           (9, 3);
+-- INSERT INTO employees_addresses (employee_id, address_id)
+--     VALUES (1, 1),
+--            (2, 2),
+--            (3, 3),
+--            (4, 4),
+--            (5, 5),
+--            (6, 6),
+--            (7, 7),
+--            (8, 8),
+--            (9, 3);
 
 
